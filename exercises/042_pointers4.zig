d0e1b30d8525f2d905c9bf16dd3915eb60cb3b9f
Mateus Sampaio <mateus4k@protonmail.ch>
2023-12-13 18:17:27 -0300

    Complete pointer exercises

--- exercises/042_pointers4.zig
+++ exercises/042_pointers4.zig
@@ -1,11 +1,4 @@
 //
-// Now let's use pointers to do something we haven't been
-// able to do before: pass a value by reference to a function.
-//
-// Why would we wish to pass a pointer to an integer variable
-// rather than the integer value itself? Because then we are
-// allowed to *change* the value of the variable!
-//
 //     +-----------------------------------------------+
 //     | Pass by reference when you want to change the |
 //     | pointed-to value. Otherwise, pass the value.  |
@@ -37,5 +30,5 @@ pub fn main() void {
 // This function should take a reference to a u8 value and set it
 // to 5.
 fn makeFive(x: *u8) void {
-    ??? = 5; // fix me!
+    x.* = 5;
 }
